19624dd3d54a777171341146c60cbd160d367867
Daniel Fox <13392079@students.lincoln.ac.uk>
2015-03-31 15:51:53 +0100

    Siege Mini-game Code Files
    Destroy goes on a blank object and removes one health each time you
    click the player.
    SiegeTower goes on the siege object and sets it's health and speed along
    with any colliders.

---
+++ Destroy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	public int clickDamage = 1;
+	GameObject seige;
+	SeigeTower seigeHealth;
+
+	// Use this for initialization
+	void Start () {
+	
+		seige = GameObject.FindGameObjectWithTag("Player");
+		seigeHealth = seige.GetComponent <SeigeTower> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero);
+			if (hit.collider != null) 
+			{
+				if (seigeHealth.currentHealth > 0) 
+				{	
+					// ... damage the player.
+					seigeHealth.TakeDamage (clickDamage);
+				}
+			}
+		}
+	}
+}
---
+++ SeigeTower.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeigeTower : MonoBehaviour {
+
+	public int startingHealth = 10;
+	public int currentHealth;
+	public float speed = 10;
+
+	public GameObject seige;
+	GameObject wall;
+	void Awake ()
+	{
+		seige = GameObject.FindGameObjectWithTag("Player");
+		wall = GameObject.FindGameObjectWithTag ("Finish");
+		currentHealth = startingHealth;
+	}
+	// Update is called once per frame
+	void Update() 
+	{
+		rigidbody2D.velocity = new Vector2(speed,0);
+
+		if (currentHealth == 0)
+		{
+			Destroy(gameObject);
+			Debug.Log ("DeadClick");
+			Application.Quit();
+		}
+	}
+
+	public void TakeDamage (int amount)
+	{
+		
+		// Reduce the current health by the damage amount.
+		currentHealth -= amount;
+
+		if (currentHealth <= 0)
+		{
+			Destroy(gameObject);
+			Debug.Log ("DeadClick");
+			Application.Quit();
+		}
+	}
+		
+		void OnTriggerEnter2D (Collider2D other)
+		{
+			if(other.gameObject == wall)
+		{
+			Debug.Log ("Dead");
+			Application.Quit();
+		}
+	}
+}
